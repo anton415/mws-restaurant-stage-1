8c25fe93005d400185a5dc9526c71acbc7f0f210
Anton Serdyuchenko <anton415@gmail.com>
2018-11-11 21:13:38 +0300

    Improve service worker.

--- serviceworker.js
+++ serviceworker.js
@@ -1,7 +1,57 @@
+var CACHE_NAME = "cache_v1";
+var CACHED_URLS = [
+  "/index.html",
+  "/restaurant.html",
+  "/js/dbhelper.js",
+  "/js/main.js",
+  "/js/restaurant_info.js",
+  "/img/1.jpg",
+  "/img/2.jpg",
+  "/img/3.jpg",
+  "/img/4.jpg",
+  "/img/5.jpg",
+  "/img/6.jpg",
+  "/img/7.jpg",
+  "/img/8.jpg",
+  "/img/9.jpg",
+  "/img/10.jpg",
+  "/img/favicon.png",
+  "/data/restaurants.json",
+  "/css/styles.css"
+];
+
 self.addEventListener("install", function(event) {
   event.waitUntil(
-    caches.open("gih-cache").then(function(cache) {
-      return cache.add("/index.html");
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHED_URLS);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Version of caches.
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (CACHE_NAME !== cacheName && cacheName.startsWith("cache_v1")) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
